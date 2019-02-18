41655d7436bf75abbf7824f9fe4a503aa32f8ea8
Jan Buschtöns <jan.buschtoens@clark.de>
2019-02-18 14:42:40 +0100

    fix(ember): add override for `Router.map(...)`

--- packages/eslint-config-ember/index.js
+++ packages/eslint-config-ember/index.js
@@ -6,5 +6,16 @@ module.exports = {
     '@clarkapp/browser',
     'plugin:ember/recommended',
     'plugin:ember-best-practices/recommended'
+  ],
+  overrides: [
+    {
+      /**
+       * This override avoids a false positive for `Router.map(...)`.
+       */
+      files: ['router.js', 'router.ts'],
+      rules: {
+        'array-callback-return': 'off'
+      }
+    }
   ]
 };
