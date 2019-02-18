12972c4adfc4f96cbbee3c9d7a36e81bd6456eb2
Jan Buschtöns <jan.buschtoens@clark.de>
2019-02-18 19:40:12 +0100

    fix(ember): disable `ember-best-practices/require-dependent-keys`
    https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/issues/103

--- packages/eslint-config-ember/index.js
+++ packages/eslint-config-ember/index.js
@@ -7,6 +7,14 @@ module.exports = {
     'plugin:ember/recommended',
     'plugin:ember-best-practices/recommended'
   ],
+  rules: {
+    /**
+     * Disabled, because it reports false positives for `@computed('foo')`.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/issues/103
+     */
+    'ember-best-practices/require-dependent-keys': 'off'
+  },
   overrides: [
     {
       /**
