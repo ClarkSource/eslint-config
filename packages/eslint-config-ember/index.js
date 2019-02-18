4b4bf3f082cd29b80ff8a3680e340ab6c26b1bef
Jan Buschtöns <jan.buschtoens@clark.de>
2019-02-18 21:23:07 +0100

    fix(ember): add exemptions for ember-css-modules

--- packages/eslint-config-ember/index.js
+++ packages/eslint-config-ember/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const {
+  DEFAULT_IGNORED_PROPERTIES
+} = require('eslint-plugin-ember/lib/rules/avoid-leaking-state-in-ember-objects');
+
 module.exports = {
   plugins: ['ember', 'ember-best-practices'],
   extends: [
@@ -13,7 +17,20 @@ module.exports = {
      *
      * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/issues/103
      */
-    'ember-best-practices/require-dependent-keys': 'off'
+    'ember-best-practices/require-dependent-keys': 'off',
+
+    /**
+     * Avoids leaking state in `.extend({ ... })` blocks. Adds exemptions for
+     * `ember-css-modules`.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/avoid-leaking-state-in-ember-objects.md
+     */
+    'avoid-leaking-state-in-ember-objects': [
+      'error',
+      ...DEFAULT_IGNORED_PROPERTIES,
+      'localClassNames',
+      'localClassNameBindings'
+    ]
   },
   overrides: [
     {
