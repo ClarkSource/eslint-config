a0c727f26307614c007b519433a239f261659c18
Jan Buschtöns <jan.buschtoens@clark.de>
2019-03-12 13:20:47 +0100

    feat(ember): improve linting rules, remove duplicates

--- packages/eslint-config-ember/index.js
+++ packages/eslint-config-ember/index.js
@@ -14,8 +14,96 @@ module.exports = {
   ],
   rules: {
     /**
-     * Disabled, because it reports false positives for `@computed('foo')`.
+     * Ember does not allow deeply-nested computed property dependent keys with
+     * `@each`, like `todos.@each.owner.name`.
      *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-deeply-nested-dependent-keys-with-each.md
+     */
+    'ember/no-deeply-nested-dependent-keys-with-each': 'error',
+
+    /**
+     * Makes sure that you use the correct order for Ember's debug functions:
+     *
+     * - `assert(description: string, condition: boolean)`
+     * - `warn(description: string, condition: boolean)`
+     * - `deprecate(description: string, condition: boolean)`
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-invalid-debug-function-arguments.md
+     */
+    'ember/no-invalid-debug-function-arguments': 'error',
+
+    /**
+     * While `this._super()` is the only way to invoke an overridden method in
+     * an `EmberObject.extend`-style class, the `_super` method doesn't work
+     * properly when using native class syntax.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/tree/master/docs/rules/no-ember-super-in-es-classes.md
+     */
+    'ember/no-ember-super-in-es-classes': 'error',
+
+    /**
+     * When defining a route, it's not necessary to specify the `path` option,
+     * if it matches the route name.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-unnecessary-route-path-option.md
+     */
+    'ember/no-unnecessary-route-path-option': 'error',
+
+    /**
+     * Do not import from a test file (a file ending in `-test.js`) in another
+     * test file. Doing so will cause the module and tests from the imported
+     * file to be executed again.
+     * Similarly, test files should not have any exports.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-test-import-export.md
+     */
+    'ember/no-test-import-export': 'error',
+
+    /**
+     * Use `await` instead of `andThen` test wait helper
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-test-and-then.md
+     */
+    'ember/no-test-and-then': 'error',
+
+    /**
+     * Prohibits the usage of jQuery. This is a hard error, since modern Ember
+     * apps don't ship with jQuery by default. If you're dealing with legacy
+     * code, we recommend setting this to `warn`.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/tree/master/docs/rules/no-jquery.md
+     */
+    'ember/no-jquery': 'error',
+
+    /**
+     * Prohibits creating mixins, as they are a bad practice, don't play well
+     * with native ES classes and will probably be deprecated at some point.
+     *
+     * Use decorators or a composable pattern instead.
+     */
+    'ember/no-new-mixins': 'error',
+
+    /**
+     * Since Ember 3.4+ this rule is not required any more, as the run loop is
+     * not starts automatically.
+     *
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/jquery-ember-run.md
+     */
+    'ember/jquery-ember-run': 'off',
+
+    /**
+     * Don't enforce it, but nudge devs to use ember-lifeline or something
+     * similar to avoid "set after destroy" errors, when using `@ember/run`.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/guides/rules/require-ember-lifeline.md
+     */
+    'ember-best-practices/require-ember-lifeline': 'warn',
+
+    /**
+     * Disabled, because it reports false positives for `@computed('foo')` and
+     * would also disallow the lazily computed cache pattern.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/guides/rules/require-dependent-keys.md
      * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/issues/103
      */
     'ember-best-practices/require-dependent-keys': 'off',
@@ -59,12 +147,52 @@ module.exports = {
     'ember/no-attrs-snapshot': 'error',
 
     /**
-     * Since Ember 3.4+ this rule is not required any more, as the run loop is
-     * not starts automatically.
+     * Disables `ember-best-practices/no-attrs` in favor of just
+     * `ember/no-attrs-in-components`, which is the same, but slightly more
+     * sophisticated.
      *
-     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/jquery-ember-run.md
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/lib/rules/no-attrs.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/lib/rules/no-attrs-in-components.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-attrs-in-components.md
      */
-    'ember/jquery-ember-run': 'off'
+    'ember-best-practices/no-attrs': 'off',
+    'ember/no-attrs-in-components': 'error',
+
+    /**
+     * Disables `ember-best-practices/no-broken-super-chain` in favor of just
+     * `ember/require-super-in-init`, which is the same, but slightly more
+     * sophisticated.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/lib/rules/no-broken-super-chain.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/lib/rules/require-super-in-init.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/require-super-in-init.md
+     */
+    'ember-best-practices/no-broken-super-chain': 'off',
+    'ember/require-super-in-init': 'error',
+
+    /**
+     * Disables `ember-best-practices/no-global-jquery` in favor of just
+     * `ember/require-super-in-init`, which is the same, but slightly more
+     * sophisticated.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/lib/rules/no-global-jquery.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/lib/rules/no-global-jquery.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-global-jquery.md
+     */
+    'ember-best-practices/no-global-jquery': 'off',
+    'ember/no-global-jquery': 'error',
+
+    /**
+     * Disables `ember-best-practices/no-send-action` in favor of just
+     * `ember/closure-actions`, which is the same, but slightly more
+     * sophisticated.
+     *
+     * @see https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/blob/master/lib/rules/no-send-action.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/lib/rules/closure-actions.js
+     * @see https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/closure-actions.md
+     */
+    'ember-best-practices/no-send-action': 'off',
+    'ember/closure-actions': 'error'
   },
   overrides: [
     {
