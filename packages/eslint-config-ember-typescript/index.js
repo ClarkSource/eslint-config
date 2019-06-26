c78e6562cedd6585d440f04fc683e4d00b4d0819
Jan Buschtöns <jan.buschtoens@clark.de>
2019-06-26 13:56:24 +0200

    fix(*-typescript): [eslint v6] overrides

--- packages/eslint-config-ember-typescript/index.js
+++ packages/eslint-config-ember-typescript/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = {
-  extends: ['@clark/ember', '@clark/typescript']
+  extends: ['@clark/ember', '@clark/typescript'],
+
+  /**
+   * Since the order of application is `ember` and _then_ `typescript`, rules
+   * set in `typescript`, override overrides in `ember`. While we in general
+   * want any regular rules in `typescript` to override any regular rules in
+   * `ember`, we want the overrides in `ember` to take effect.
+   *
+   * To achieve this, we include the overrides again.
+   *
+   * @see https://eslint.org/docs/user-guide/migrating-to-6.0.0#-overrides-in-an-extended-config-file-can-now-be-overridden-by-a-parent-config-file
+   */
+  overrides: require('@clark/eslint-config-ember').overrides
 };
--- packages/eslint-config-node-typescript/index.js
+++ packages/eslint-config-node-typescript/index.js
@@ -23,5 +23,17 @@ module.exports = {
   rules: {
     'node/no-unsupported-features/es-builtins': 'off',
     'node/no-unsupported-features/es-syntax': 'off'
-  }
+  },
+
+  /**
+   * Since the order of application is `node` and _then_ `typescript`, rules
+   * set in `typescript`, override overrides in `node`. While we in general
+   * want any regular rules in `typescript` to override any regular rules in
+   * `node`, we want the overrides in `node` to take effect.
+   *
+   * To achieve this, we include the overrides again.
+   *
+   * @see https://eslint.org/docs/user-guide/migrating-to-6.0.0#-overrides-in-an-extended-config-file-can-now-be-overridden-by-a-parent-config-file
+   */
+  overrides: require('@clark/eslint-config-node').overrides
 };
