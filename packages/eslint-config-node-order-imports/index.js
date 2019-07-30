b2cdadd580466f367450b01eeba4d461728782ac
Jan Buschtöns <jan.buschtoens@clark.de>
2019-07-30 11:03:14 +0200

    fix(node-order-imports): fix std module regex

--- packages/eslint-config-node-order-imports/index.js
+++ packages/eslint-config-node-order-imports/index.js
@@ -12,7 +12,7 @@ module.exports = {
         newlinesBetween: 'always',
         groups: [
           [
-            '/^(assert|async_hooks|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|http2|https|inspector|module|net|os|path|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|zli)\\/?/'
+            '/^(assert|async_hooks|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|http2|https|inspector|module|net|os|path|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|zli)(\\/.+)?$/'
           ],
           ['module'],
           ['absolute'],
