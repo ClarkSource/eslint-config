bf029937a2e51dce8ffb9d34266eceaefdc7890b
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2019-09-09 16:55:38 +0300

    fix(eslint-config-ember-typescript): Fix the 'test' sub-config's require

--- packages/eslint-config-ember-typescript/test.js
+++ packages/eslint-config-ember-typescript/test.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [require.resolve('./')],
   rules: {
-    ...require.resolve('@clark/eslint-config-ember/test').rules,
+    ...require('@clark/eslint-config-ember/test').rules,
     '@typescript-eslint/no-empty-function': 'off'
   }
 };
