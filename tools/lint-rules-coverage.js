808b17db184429f1cf4fea7393c94ec388754878
Jan Buschtöns <jan.buschtoens@clark.de>
2020-02-24 22:28:14 +0100

    test: check that all base overridable base rules are overridden

---
+++ tools/lint-rules-coverage.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const { resolve } = require('path');
+
+const { CLIEngine } = require('eslint');
+const {
+  normalizePackageName,
+  getNamespaceFromTerm
+} = require('eslint/lib/shared/naming');
+
+const [baseConfigPath, ...additiveConfigNames] = process.argv.slice(2);
+const baseConfig = require(resolve(baseConfigPath));
+// eslint-disable-next-line node/no-unsupported-features/es-builtins
+const additiveConfigs = Object.fromEntries(
+  additiveConfigNames.map(n => {
+    const normalizedName = normalizePackageName(n, 'eslint-plugin');
+    return [normalizedName, require(normalizedName)];
+  })
+);
+
+const cliEngine = new CLIEngine({ baseConfig, useEslintrc: false });
+const effectiveBaseConfig = cliEngine.getConfigForFile('foo.js');
+
+const baseRuleNames = Object.keys(effectiveBaseConfig.rules);
+
+const filterAdditiveRules = predicate =>
+  // eslint-disable-next-line node/no-unsupported-features/es-builtins
+  Object.fromEntries(
+    Object.entries(additiveConfigs).map(([name, config]) => [
+      name,
+      Object.keys(config.rules).filter(ruleName =>
+        predicate(ruleName, `${getNamespaceFromTerm(name)}${ruleName}`)
+      )
+    ])
+  );
+
+const isAdditiveRulesEmpty = rules =>
+  Object.values(rules).every(v => !v || !v.length);
+
+const missingOverrides = filterAdditiveRules(
+  (name, scopedNamed) =>
+    baseRuleNames.includes(name) && !baseRuleNames.includes(scopedNamed)
+);
+
+const unusedRules = filterAdditiveRules(
+  (name, scopedNamed) =>
+    !baseRuleNames.includes(name) && !baseRuleNames.includes(scopedNamed)
+);
+
+console.log({ missingOverrides, unusedRules });
+
+if (!isAdditiveRulesEmpty(missingOverrides))
+  throw new Error(
+    `There are some overrides available for rules defined in the base config, that are not used.`
+  );
