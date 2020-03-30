6a88c1fbf8a8bc374fdda9710bf8a601d9e68c20
Jan Buschtöns <jan.buschtoens@clark.de>
2020-03-30 21:34:41 +0200

    fix(base): add `trailingComma: none`

--- packages/eslint-config/lib/last.js
+++ packages/eslint-config/lib/last.js
@@ -6,6 +6,6 @@ module.exports = {
     /**
      * This enables the default prettier formatting, but with single quotes.
      */
-    'prettier/prettier': ['error', { singleQuote: true }]
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'none' }]
   }
 };
