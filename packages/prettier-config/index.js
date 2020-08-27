c44720e40c909316d4b67ca6ec845e77e3bde3d5
Jan Buschtöns <jan.buschtoens@clark.de>
2020-08-27 15:11:23 +0200

    fix(prettier): use double quotes for hbs, html

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,5 +3,14 @@
 module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
-  trailingComma: 'none' // @TODO: switch to 'all'
+  trailingComma: 'none', // @TODO: switch to 'all'
+
+  overrides: [
+    {
+      files: ['*.hbs', '*.html'],
+      options: {
+        singleQuote: false
+      }
+    }
+  ]
 };
