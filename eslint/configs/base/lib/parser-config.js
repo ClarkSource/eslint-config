b8c09918e6c04c79a58056bdca273ca3e0884421
Jan Buschtöns <jan.buschtoens@clark.de>
2021-08-03 02:37:29 +0200

    feat(eslint): enable `impliedStrict`

--- eslint/configs/base/lib/parser-config.js
+++ eslint/configs/base/lib/parser-config.js
@@ -4,6 +4,9 @@ module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2021,
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
     sourceType: 'module',
     requireConfigFile: false,
   },
